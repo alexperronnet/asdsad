f074b8772b67956131a94b71207bcce6e577e5db
alexperronnet <github.ayfb9@lxprnt.com>
2022-09-08 06:24:06 +0200

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,9 @@ cd dist
 # Initialize a git repository
 git init
 
+# Add changes to git
+git add -A
+
 # Check if main branch exists
 if git ls-remote --exit-code --heads origin deploy; then
   git checkout main
@@ -16,13 +19,10 @@ else
   git checkout -b main
 fi
 
-# Add changes to git
-git add -A
-
 # Commit changes
 git commit -m 'Deploy'
 
 # Push to deploy branch
-git push -f git@github.com:${USERNAME}/${REPO}.git main:deploy
+git push -f git@github.com:alexperronnet/openclassrooms-p4-gameon.git main:deploy
 
 cd -
